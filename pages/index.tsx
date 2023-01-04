d11754ad75cde3ca038ee2c52bc64479dcd3c01b
Noubar Jack <noubar@kmmrce.com>
2023-01-04 15:20:26 +0400

    fix(docs): added some comments for clarification

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '../styles/Home.module.css';
 
+// This is the only place where you can use the `Inter` font.
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
